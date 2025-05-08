7146597a6d207303ca5e46fd2d63db5e6e6ca999
MP-OPTI <44803291+MP-OPTI@users.noreply.github.com>
2025-05-08 18:46:19 +0200

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import app from './firebase'; // Uncomment to use Firebase instance
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">err.dev</h1>
+      <p className="text-lg text-gray-600">Your Firebase + React + Tailwind starter template.</p>
+    </div>
+  );
+}
+
+export default App;
